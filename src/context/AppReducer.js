fe21939c02e68b335fab16522604d6dd81a51d35
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-20 19:04:27 -0300

    last states and actions

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,7 +1,19 @@
 //file that we specified state changes
 export default (state, action) => {
     switch(action.type) {
-        default:
+        case 'DELETE_TRANSACTION': 
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+            case 'ADD_TRANSACTION':
+                return {
+                    ...state,
+                    transactions: [action.payload, ...state.transactions]
+
+                }
+            default:
             return state;
     }
+    
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, {createContext, useReducer} from "react";
 import AppReducer from './AppReducer';
 //initial state
 const initalState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions: [],
 }
 
 //creating context 
@@ -17,8 +12,27 @@ export const GlobalContext = createContext(initalState);
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initalState);
 
+    //actions
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    
+
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        addTransaction,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
